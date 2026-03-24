02f2b09a5825282c5fce2fa50e6b91e0858d6821
Carlos Andres Galindo <andres.galindo@sidi.mx>
2026-03-23 22:35:05 -0600

    fix: exclude prisma/scripts/pages.backup from build, clean git tracking

--- prisma/migrations/20251012001318_add_plan_model/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Plan" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "price" REAL NOT NULL,
-    "credits" INTEGER NOT NULL
-);
--- prisma/migrations/20251012161700_make_fields_optional/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Booking" ADD COLUMN "date" DATETIME;
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "birthdate" DATETIME;
-ALTER TABLE "User" ADD COLUMN "name" TEXT;
